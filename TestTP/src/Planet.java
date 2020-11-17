ea56c5e9ef19c96952eb3ac9ba5ee078fef68859
Rabat IT Center Autumn School <mahmoud.elhamlaoui@gmail.com>
2020-11-17 16:28:26 +0100

    Chgt planet

--- TestTP/src/Planet.java
+++ TestTP/src/Planet.java
@@ -3,6 +3,7 @@ public enum Planet {
     MERCURY (3.303e+23, 2.4397e6),
     VENUS   (4.869e+24, 6.0518e6),
     EARTH   (5.976e+24, 6.37814e6),
+    
     MARS    (6.421e+23, 3.3972e6),
     JUPITER (1.9e+27,   7.1492e7),
     SATURN  (5.688e+26, 6.0268e7),
